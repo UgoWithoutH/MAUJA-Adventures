a7abe959043fff280ccd8a6d26d782ea880ab7b9
mawissocq <maxime.wissocq@etu.uca.fr>
2021-11-20 18:03:20 +0100

    Ajout classe Camera

--- source/src/main/java/com/mauja/maujaadventures/modele/Camera.java
+++ source/src/main/java/com/mauja/maujaadventures/modele/Camera.java
@@ -1,2 +1,34 @@
-package com.mauja.maujaadventures.modele;public class Camera {
+package com.mauja.maujaadventures.modele;
+
+public class Camera {
+    private int positionCameraX;
+    private int positionCameraY;
+
+    //private Entite cible;
+
+    public Camera(int positionCameraX,int positionCameraY){
+        this.positionCameraX=positionCameraX;
+        this.positionCameraY=positionCameraY;
+    }
+
+    public void deplacementCamera(int deplaceX,int deplaceY){
+        positionCameraX+=deplaceX;
+        positionCameraY+=deplaceY;
+    }
+
+    public int getPositionCameraX() {
+        return positionCameraX;
+    }
+
+    public void setPositionCameraX(int positionCameraX) {
+        this.positionCameraX = positionCameraX;
+    }
+
+    public int getPositionCameraY() {
+        return positionCameraY;
+    }
+
+    public void setPositionCameraY(int positionCameraY) {
+        this.positionCameraY = positionCameraY;
+    }
 }
