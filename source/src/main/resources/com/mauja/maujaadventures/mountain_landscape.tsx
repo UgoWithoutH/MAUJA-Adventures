ea112d727c63dcb2c7f3b2fc91e298ee600fda89
ugvignon <ugo.vignon@etu.uca.fr>
2021-12-06 19:01:06 +0100

    ajout des tilesets pour Maxime

---
+++ source/src/main/resources/com/mauja/maujaadventures/mountain_landscape.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="mountain_landscape" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="tilesets/2d loast garden zelda style tiles resized/mountain_landscape.png" width="512" height="512"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="18.625" y="16.375" width="13.375" height="15.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.125" y="13.875" width="32" height="18.25"/>
+  </objectgroup>
+ </tile>
+</tileset>
