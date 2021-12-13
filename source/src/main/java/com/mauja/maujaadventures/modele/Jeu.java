9177665c13fbdb038a82f5faf45a955c12fc3347
mawissocq <maxime.wissocq@etu.uca.fr>
2021-12-13 11:47:21 +0100

    Remplacement des valeurs brut de Jeu pour pouvoir charger des cartes de différentes tailles sans toucher au code

--- source/src/main/java/com/mauja/maujaadventures/modele/Jeu.java
+++ source/src/main/java/com/mauja/maujaadventures/modele/Jeu.java
@@ -98,7 +98,7 @@ public class Jeu {
     /**
      * Méthode de la boucle de jeu du programme
 
-     * @param input Correspond à une liste detouche taper par l'utilisateur
+     * @param input Correspond à une liste de touches taper par l'utilisateur
      * @param e Entité que l'on fait bouger
      * @author Tremblay Jeremy, Vignon Ugo, Viton Antoine, Wissocq Maxime, Coudour Adrien
      */
@@ -126,9 +126,9 @@ public class Jeu {
             {
                 contexteGraphique.effacer(new Position(0, 0), new Dimension(1000, 1000));
                 for (int k = 0; k < nombreCalques; k++) {
-                    for (int i = 0; i < 80; i++) {
-                        for (int j = 0; j < 80; j++) {
-                            Tuile tuile = carte.getListeDeCalques().get(k).getListeDeTuiles().get(i * 80 + j);
+                    for (int i = 0; i < carte.getDimension().getLargeur(); i++) {
+                        for (int j = 0; j < carte.getDimension().getHauteur(); j++) {
+                            Tuile tuile = carte.getListeDeCalques().get(k).getListeDeTuiles().get(i * (int)carte.getDimension().getLargeur() + j);
                             if (tuile.getId() >= 1) {
                                 contexteGraphique.dessiner(
                                         lesImages.get(tuile.getId()),
@@ -148,10 +148,9 @@ public class Jeu {
                 positionPerso = lesTuilesCourantes.get((int) (positionY * largeurCarte + positionX)).getCollision();
 
                 if (input.contains("LEFT")) {
-                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32)) * 80 +
+                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32)) * (int)carte.getDimension().getLargeur() +
                             (e.getPosition().getPositionX() / 32) + 0)).getCollision() == null) {
                         deplaceur.deplace(e,e.getPosition().getPositionX() - 3, e.getPosition().getPositionY());
-                        //if (0 + e.getPosition().getPositionY() > 100) {
                         if (!(camera.getPositionCameraX()<=0) &&
                                 (e.getPosition().getPositionX() <= carte.getDimension().getLargeur()*20 +
                                         Jeu.this.getGraphicsContext().getCanvas().getWidth() / 2)){
@@ -161,26 +160,23 @@ public class Jeu {
                 }
 
                 if (input.contains("RIGHT")) {
-                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32)) * 80 +
+                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32)) * (int)carte.getDimension().getLargeur() +
                             (e.getPosition().getPositionX() / 32) + 1)).getCollision() == null) {
                         deplaceur.deplace(e, e.getPosition().getPositionX() + 3, e.getPosition().getPositionY());
-                        //if ((carte.getDimension().getLargeur()*80) - (e.getPosition().getPositionX()) > 100){
-                        if (((camera.getPositionCameraX() <= carte.getDimension().getLargeur()*20)) &&
+                        if (((camera.getPositionCameraX() <= carte.getDimension().getLargeur() * 20)) &&
                                 (e.getPosition().getPositionX() >= Jeu.this.getGraphicsContext().getCanvas().getWidth() / 2)) {
 
                             camera.deplacementCamera(3, 0);
-                            System.out.println("camx: " + camera.getPositionCameraX());
-                            System.out.println("dimx: " + carte.getDimension().getLargeur()*20);
                         }
                     }
                 }
 
                 if (input.contains("UP")) {
-                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32)) * 80 +
+                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32)) * (int)carte.getDimension().getLargeur() +
                             (e.getPosition().getPositionX() / 32) + 0)).getCollision() == null) {
                         deplaceur.deplace(e, e.getPosition().getPositionX(),e.getPosition().getPositionY() - 3);
-                        if (!(camera.getPositionCameraY()<=0) &&
-                                (e.getPosition().getPositionY() <= carte.getDimension().getHauteur()*20 + 5*32 +
+                        if (!(camera.getPositionCameraY() <= 0) &&
+                                (e.getPosition().getPositionY() <= carte.getDimension().getHauteur() * 22 +
                                         Jeu.this.getGraphicsContext().getCanvas().getHeight() / 2)) {
                             camera.deplacementCamera(0, -3);
                             System.out.println(camera.getPositionCameraY());
@@ -189,11 +185,11 @@ public class Jeu {
                 }
 
                 if (input.contains("DOWN")) {
-                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32) + 1) * 80 +
+                    if (carte.getListeDeCalques().get(0).getListeDeTuiles().get((int)((int)((e.getPosition().getPositionY() / 32) + 1) * (int)carte.getDimension().getLargeur() +
                             (e.getPosition().getPositionX() / 32) + 0)).getCollision() == null) {
                         deplaceur.deplace(e, e.getPosition().getPositionX(), e.getPosition().getPositionY() + 3);
-                        if ((carte.getDimension().getLargeur()*80) - (e.getPosition().getPositionY()) > 100 &&
-                        (camera.getPositionCameraY() <= carte.getDimension().getHauteur()*20 + 5*32 &&
+                        if ((carte.getDimension().getLargeur() * carte.getDimension().getLargeur()) - (e.getPosition().getPositionY()) > 100 &&
+                        (camera.getPositionCameraY() <= carte.getDimension().getHauteur() * 22 &&
                                 (e.getPosition().getPositionY() >= Jeu.this.getGraphicsContext().getCanvas().getHeight() / 2))) {
                             camera.deplacementCamera(0, 3);
                         }
