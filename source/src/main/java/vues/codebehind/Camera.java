c4044729a1284ddd01304b251a5f378b66def266
mawissocq <maxime.wissocq@etu.uca.fr>
2022-02-14 08:06:04 +0100

    Avancement Caméra de la vue

--- source/src/main/java/vues/codebehind/Camera.java
+++ source/src/main/java/vues/codebehind/Camera.java
@@ -1,2 +1,19 @@
-package vues.codebehind;public class Camera {
+package vues.codebehind;
+
+import com.mauja.maujaadventures.entites.Entite;
+import com.mauja.maujaadventures.logique.Dimension;
+import com.mauja.maujaadventures.logique.Position;
+
+public abstract class Camera {
+    protected Dimension zoneObservable;
+    protected Dimension milieu;
+    protected Position position;
+    public Camera(Dimension zoneObservable) {
+        this.zoneObservable = zoneObservable;
+        milieu = new Dimension(zoneObservable.getLargeur(), zoneObservable.getHauteur());
+
+    }
+
+    public abstract void centrerSurEntite(Entite entite);
+
 }
