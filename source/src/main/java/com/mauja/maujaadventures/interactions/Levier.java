876be5bc1a20f495906dc428ed05d0a1582bd2a9
anviton <antoine.viton2@gmail.com>
2022-02-26 01:04:58 +0100

    Création et utilisation d'annotations pour le parsage, modification du parsage des éléments interactifs notamment lors de leur création (ne fonctionne que sur les ennemis et leviers pour le moment)

--- source/src/main/java/com/mauja/maujaadventures/interactions/Levier.java
+++ source/src/main/java/com/mauja/maujaadventures/interactions/Levier.java
@@ -1,15 +1,18 @@
 package com.mauja.maujaadventures.interactions;
 
+import com.mauja.maujaadventures.annotations.ConstructDef;
+import com.mauja.maujaadventures.annotations.Param;
 import com.mauja.maujaadventures.logique.Position;
 
 public class Levier extends ElementInteractif{
     public Position position;
     public boolean active;
 
-
-    public Levier(Double x, Double y) {
+    @ConstructDef
+    public Levier(@Param(nom = "x") Double x, @Param(nom = "y") Double y,
+                  @Param(nom = "active", classe = Boolean.class) boolean active) {
         this.position = new Position(x,y);
-        this.active = false;
+        this.active = active;
     }
 
     public Position getPosition() {
